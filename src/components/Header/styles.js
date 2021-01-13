f07edf547a3ebdec5b0e9f9658edc5a33ad5a4cb
Bruno <brunoalnascimento@gmail.com>
2021-01-13 04:09:49 +0100

    implements header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,20 @@
+import { css } from 'styled-components';
+
+export default css`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  h1 {
+    position: absolute;
+    font-size: var(--font-size-xlg);
+    font-weight: 900;
+    color: var(--gray-primary);
+    text-align: center;
+  }
+
+  svg {
+    fill: var(--white);
+  }
+`;
