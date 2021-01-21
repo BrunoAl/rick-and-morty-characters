6d78e9ae201767f07a1b9c90b892c16f67c93b3b
Bruno <brunoalnascimento@gmail.com>
2021-01-21 17:32:33 +0100

    more reliable input testing

--- src/components/CharactersSearchForm/index.test.js
+++ src/components/CharactersSearchForm/index.test.js
@@ -1,18 +1,19 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CharactersSearchForm from './index';
 
-const searchTerm = 'R';
+const searchTerm = 'Random input value';
 
 describe('CharactersSearchForm', () => {
   it('Should fill the input field and click submit ', () => {
     let inputValue = '';
     const handleSubmit = jest.fn();
     const handleChange = jest.fn(value => {
-      inputValue = value;
+      inputValue = `${inputValue}${value}`;
     });
 
-    render(
+    const { rerender } = render(
       <CharactersSearchForm
         label="Search by dimension "
         inputExample="(e.g. Dimension C-137, Post-Apocalyptic Dimension, Replacement Dimension)"
@@ -22,10 +23,27 @@ describe('CharactersSearchForm', () => {
       />,
     );
 
-    userEvent.type(screen.getByRole('textbox', { name: /search by dimension/i }), searchTerm);
-    userEvent.click(screen.getByRole('button', { name: /search/i }));
+    const input = screen.getByRole('textbox', { name: /search by dimension/i });
+    const submitButton = screen.getByRole('button', { name: /search/i });
 
-    expect(handleChange).toHaveBeenCalledWith(searchTerm);
+    expect(input.value).toBe('');
+    expect(handleSubmit).toHaveBeenCalledTimes(0);
+
+    userEvent.type(input, searchTerm);
+
+    rerender(
+      <CharactersSearchForm
+        label="Search by dimension "
+        inputExample="(e.g. Dimension C-137, Post-Apocalyptic Dimension, Replacement Dimension)"
+        onChange={handleChange}
+        value={inputValue}
+        onSubmit={handleSubmit}
+      />,
+    );
+
+    userEvent.click(submitButton);
+    expect(input.value).toBe(searchTerm);
     expect(handleSubmit).toHaveBeenCalledTimes(1);
+    expect(handleSubmit).toHaveBeenCalledWith(searchTerm);
   });
 });
