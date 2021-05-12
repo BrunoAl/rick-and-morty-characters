fad31914fe127b8eaaa6e2109cc0f1e1e2fcf873
Bruno <brunoalnascimento@gmail.com>
2021-05-12 11:40:22 +0200

    fixes typo

--- src/utils/APIMockData/index.js
+++ src/utils/APIMockData/index.js
@@ -1,4 +1,4 @@
 import characters from './characters.json';
 import dimensions from './dimensions.json';
 
-export { characters as charactersData, dimensions as dimentionsData };
+export { characters as charactersData, dimensions as dimensionsData };
--- src/utils/serverHandlers.js
+++ src/utils/serverHandlers.js
@@ -1,17 +1,17 @@
 import { rest } from 'msw';
-import { charactersData, dimentionsData } from '../utils/APIMockData/';
+import { charactersData, dimensionsData } from '../utils/APIMockData/';
 
 const BASE_API = '${BASE_API}';
 
 const handlers = [
   rest.get(`${BASE_API}/dimension`, (req, res, ctx) => {
-    return res(ctx.json(dimentionsData));
+    return res(ctx.json(dimensionsData));
   }),
   rest.get(`${BASE_API}/episode`, (req, res, ctx) => {
-    return res(ctx.json(dimentionsData));
+    return res(ctx.json(dimensionsData));
   }),
   rest.get(`${BASE_API}/location`, (req, res, ctx) => {
-    return res(ctx.json(dimentionsData));
+    return res(ctx.json(dimensionsData));
   }),
   rest.get(`${BASE_API}/character/:ids`, (req, res, ctx) => {
     return res(ctx.json(charactersData));
